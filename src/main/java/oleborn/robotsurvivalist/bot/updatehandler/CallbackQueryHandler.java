e173b532e94a51314fcfd23d56d94a3cc2e239f9
Denis <Oleborn@mail.ru>
2025-03-12 14:09:28 +0700

    добавлена стартовая история

--- src/main/java/oleborn/robotsurvivalist/bot/updatehandler/CallbackQueryHandler.java
+++ src/main/java/oleborn/robotsurvivalist/bot/updatehandler/CallbackQueryHandler.java
@@ -1,7 +1,10 @@
 package oleborn.robotsurvivalist.bot.updatehandler;
 
 import jakarta.annotation.Resource;
+import oleborn.robotsurvivalist.bot.outputMethods.InlineKeyboardBuilder;
 import oleborn.robotsurvivalist.bot.outputMethods.OutputsMethods;
+import oleborn.robotsurvivalist.game.gamedictionary.historygame.CentralHistoryGame;
+import oleborn.robotsurvivalist.utils.UtilsMethods;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
@@ -14,7 +17,55 @@ public class CallbackQueryHandler implements Handler {
     @Override
     public void handleUpdate(Update update) {
 
-
-
+        switch (update.getCallbackQuery().getData()) {
+            case "next_history_1" -> {
+                outputsMethods.outputMessage(
+                        UtilsMethods.searchId(update),
+                        CentralHistoryGame.BRIEFING_GAME_START1.getText(),
+                        new InlineKeyboardBuilder()
+                                .addButton("Дальше", "next_history_2")
+                                .build());
+            }
+            case "next_history_2" -> {
+                outputsMethods.outputMessage(
+                        UtilsMethods.searchId(update),
+                        CentralHistoryGame.BRIEFING_GAME_START2.getText(),
+                        new InlineKeyboardBuilder()
+                                .addButton("Дальше", "next_history_3")
+                                .build());
+            }
+            case "next_history_3" -> {
+                outputsMethods.outputMessage(
+                        UtilsMethods.searchId(update),
+                        CentralHistoryGame.BRIEFING_GAME_START3.getText(),
+                        new InlineKeyboardBuilder()
+                                .addButton("Дальше", "next_history_4")
+                                .build());
+            }
+            case "next_history_4" -> {
+                outputsMethods.outputMessage(
+                        UtilsMethods.searchId(update),
+                        CentralHistoryGame.BRIEFING_GAME_START4.getText(),
+                        new InlineKeyboardBuilder()
+                                .addButton("Дальше", "next_history_5")
+                                .build());
+            }
+            case "next_history_5" -> {
+                outputsMethods.outputMessage(
+                        UtilsMethods.searchId(update),
+                        CentralHistoryGame.BRIEFING_GAME_START5.getText(),
+                        new InlineKeyboardBuilder()
+                                .addButton("Запустить робота", "start_robot")
+                                .build());
+            }
+            case "start_robot" -> {
+                outputsMethods.outputMessage(
+                        UtilsMethods.searchId(update),
+                        CentralHistoryGame.ROBOT_STARTED.getText(),
+                        new InlineKeyboardBuilder()
+                                .addButton("Начать игру", "start_game")
+                                .build());
+            }
+        }
     }
 }
--- src/main/java/oleborn/robotsurvivalist/bot/updatehandler/CommandHandler.java
+++ src/main/java/oleborn/robotsurvivalist/bot/updatehandler/CommandHandler.java
@@ -1,13 +1,32 @@
 package oleborn.robotsurvivalist.bot.updatehandler;
 
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import oleborn.robotsurvivalist.bot.outputMethods.InlineKeyboardBuilder;
+import oleborn.robotsurvivalist.bot.outputMethods.OutputsMethods;
+import oleborn.robotsurvivalist.game.gamedictionary.historygame.CentralHistoryGame;
+import oleborn.robotsurvivalist.utils.UtilsMethods;
+import oleborn.robotsurvivalist.utils.historydictionary.TextMessages;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
 @Component
+@RequiredArgsConstructor
 public class CommandHandler implements Handler {
 
-    @Override
-    public void handleUpdate(Update update) {
+    private final OutputsMethods outputsMethods;
 
+    @Override
+    @SneakyThrows
+    public void handleUpdate(Update update) {
+        if(update.getMessage().getText().equals("/start")) {
+            outputsMethods.outputMessage(
+                    UtilsMethods.searchId(update),
+                    CentralHistoryGame.START_GAME_HISTORY.getText(),
+                    new InlineKeyboardBuilder()
+                            .addButton("Дальше", "next_history_1")
+                            .build()
+            );
+        }
     }
 }
